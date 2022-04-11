9ce78b2ade3a8fca3fff8ddaf0b50f482b681843
LongYinan <lynweklm@gmail.com>
2022-04-11 16:05:21 +0800

    /GL-

--- replace-system-cl.js
+++ replace-system-cl.js
@@ -5,4 +5,4 @@ const CL_REAL_PATH = execSync('which cl').toString().trim()
 
 console.info(CL_REAL_PATH)
 
-writeFileSync('bin/cl.bat', `${CL_REAL_PATH} /GL %*`)
+writeFileSync('bin/cl.bat', `${CL_REAL_PATH} /GL- %*`)
