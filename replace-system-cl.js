a6afd6c98a56d47d8d58ed064549c879d68a8f71
LongYinan <lynweklm@gmail.com>
2022-04-17 14:57:08 +0800

    wip

--- replace-system-cl.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process')
-const { writeFileSync } = require('fs')
-
-const CL_REAL_PATH = execSync('which cl').toString().trim()
-
-console.info(CL_REAL_PATH)
-
-writeFileSync('bin/cl.bat', `${CL_REAL_PATH} /GL- %*`)
