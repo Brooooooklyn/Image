24736809eaa38237bd618b5860b12ae0ebe91bd6
LongYinan <lynweklm@gmail.com>
2022-04-22 21:41:33 +0800

    fix(image): webp encode LumaA8 and Luma8

--- packages/binding/src/webp.rs
+++ packages/binding/src/webp.rs
@@ -1,4 +1,4 @@
-use image::ColorType;
+use image::{buffer::ConvertBuffer, ColorType, DynamicImage, Rgb, Rgba};
 use napi::bindgen_prelude::*;
 
 #[inline]
@@ -45,15 +45,15 @@ pub(crate) unsafe fn lossless_encode_webp_inner(
 
 #[inline]
 pub(crate) unsafe fn encode_webp_inner(
-  input: &[u8],
+  input: &DynamicImage,
   quality_factor: u32,
   width: u32,
   height: u32,
   color_type: &image::ColorType,
 ) -> Result<(*mut u8, usize)> {
   let mut out_buf = std::ptr::null_mut();
-  let len = match color_type {
-    ColorType::Rgb8 => {
+  let len = match input {
+    DynamicImage::ImageRgb8(input) => {
       let stride = width as i32 * 3;
       libwebp_sys::WebPEncodeRGB(
         input.as_ptr(),
@@ -64,10 +64,34 @@ pub(crate) unsafe fn encode_webp_inner(
         &mut out_buf,
       )
     }
-    ColorType::Rgba8 => {
+    DynamicImage::ImageRgba8(input) => {
       let stride = width as i32 * 4;
       libwebp_sys::WebPEncodeRGBA(
         input.as_ptr(),
+        width as i32,
+        height as i32,
+        stride,
+        quality_factor as f32,
+        &mut out_buf,
+      )
+    }
+    DynamicImage::ImageLuma8(input) => {
+      let stride = width as i32 * 3;
+      let converted: image::ImageBuffer<Rgb<u8>, _> = input.convert();
+      libwebp_sys::WebPEncodeRGB(
+        converted.as_ptr(),
+        width as i32,
+        height as i32,
+        stride,
+        quality_factor as f32,
+        &mut out_buf,
+      )
+    }
+    DynamicImage::ImageLumaA8(input) => {
+      let stride = width as i32 * 4;
+      let converted: image::ImageBuffer<Rgba<u8>, _> = input.convert();
+      libwebp_sys::WebPEncodeRGBA(
+        converted.as_ptr(),
         width as i32,
         height as i32,
         stride,
