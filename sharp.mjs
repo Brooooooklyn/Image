05dd1791d9b479113563e07e7ecb765ad3021fab
LongYinan <lynweklm@gmail.com>
2023-01-13 23:44:12 +0800

    Update

--- sharp.mjs
+++ sharp.mjs
@@ -4,33 +4,12 @@ import { readFileSync, writeFileSync } from 'fs'
 
 import sharp from 'sharp'
 
-import { ChromaSubsampling, Transformer, fastResize, FastResizeFilter } from '@napi-rs/image'
+import { ChromaSubsampling, Transformer, fastResize, FastResizeFilter, ResizeFilterType } from '@napi-rs/image'
 
 // https://github.com/ianare/exif-samples/blob/master/jpg/orientation/portrait_5.jpg
 const WITH_EXIF = readFileSync('./with-exif.jpg')
 
 const NASA = readFileSync('./nasa-4928x3279.png')
-
-console.time('sharp resize')
-
-await sharp(NASA)
-  .resize(1024, 768, {
-    kernel: sharp.kernel.lanczos3,
-  })
-  .png()
-  .toBuffer()
-
-console.timeEnd('sharp resize')
-
-console.time('fast resize')
-
-fastResize(NASA, {
-  width: 1024,
-  height: 768,
-  filter: FastResizeFilter.Lanczos3,
-})
-
-console.timeEnd('fast resize')
 
 console.time('sharp webp')
 
@@ -81,3 +60,30 @@ const imageOutputAvif = await new Transformer(WITH_EXIF)
 console.timeEnd('@napi-rs/image avif')
 
 writeFileSync('output-exif.image.avif', imageOutputAvif)
+
+console.time('sharp resize')
+
+await sharp(NASA)
+  .resize(1024, 768, {
+    kernel: sharp.kernel.lanczos3,
+  })
+  .png()
+  .toBuffer()
+
+console.timeEnd('sharp resize')
+
+console.time('@napi-rs/image resize')
+
+await new Transformer(NASA).resize(1024, 768, ResizeFilterType.Lanczos3).png()
+
+console.timeEnd('@napi-rs/image resize')
+
+console.time('fast resize')
+
+fastResize(NASA, {
+  width: 1024,
+  height: 768,
+  filter: FastResizeFilter.Lanczos3,
+})
+
+console.timeEnd('fast resize')
