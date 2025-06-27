72d5e70e98fb079c0a69bfdcbd0b75e09b653c17
LongYinan <lynweklm@gmail.com>
2025-06-27 23:06:01 +0800

    feat(binding): add mimalloc

--- packages/binding/src/lib.rs
+++ packages/binding/src/lib.rs
@@ -1,5 +1,9 @@
 #![deny(clippy::all)]
 
+#[cfg(not(any(target_family = "wasm", target_arch = "arm")))]
+#[global_allocator]
+static ALLOC: mimalloc_safe::MiMalloc = mimalloc_safe::MiMalloc;
+
 pub mod avif;
 mod fast_resize;
 pub mod jpeg;
