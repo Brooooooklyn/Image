0c4ef629f688e0b680c4b502c067e2fe042862ed
LongYinan <lynweklm@gmail.com>
2025-07-24 17:27:01 +0800

    process.arch

--- website/generate-img.js
+++ website/generate-img.js
@@ -1,7 +1,7 @@
-import { execSync } from 'child_process'
-import { join } from 'path'
-import { promises as fs } from 'fs'
-import { fileURLToPath } from 'url'
+import { execSync } from 'node:child_process'
+import { join } from 'node:path'
+import { promises as fs } from 'node:fs'
+import { fileURLToPath } from 'node:url'
 
 import chalk from 'chalk'
 import fetch from 'node-fetch'
@@ -13,12 +13,13 @@ await fs.writeFile('public/img/example.mjs', await fs.readFile('../example.mjs')
 await fs.writeFile('public/img/sharp.mjs', await fs.readFile('../sharp.mjs'))
 
 if (process.env.VERCEL) {
-  const gnuBinary = await fetch(`https://unpkg.com/@napi-rs/image-linux-x64-gnu`, {
+  const arch = process.arch
+  const gnuBinary = await fetch(`https://unpkg.com/@napi-rs/image-linux-${arch}-gnu`, {
     redirect: 'follow',
     follow: 10,
   }).then((res) => res.arrayBuffer())
-  console.info(chalk.greenBright(`Installed @napi-rs/image.linux-x64-gnu, size: ${gnuBinary.byteLength}`))
-  await fs.writeFile(join(__dirname, '../../../packages/binding/image.linux-x64-gnu.node'), Buffer.from(gnuBinary))
+  console.info(chalk.greenBright(`Installed @napi-rs/image.linux-${arch}-gnu, size: ${gnuBinary.byteLength}`))
+  await fs.writeFile(join(__dirname, `../../../packages/binding/image.linux-${arch}-gnu.node`), Buffer.from(gnuBinary))
 }
 
 execSync('node example.mjs', {
