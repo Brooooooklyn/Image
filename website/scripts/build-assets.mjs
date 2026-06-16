09af26a03ab6748a5983c8e965fbc690ded22d4c
LongYinan <lynweklm@gmail.com>
2026-06-16 09:50:55 +0800

    feat(website): emit showcase-manifest.json with real asset sizes at build

--- website/scripts/build-assets.mjs
+++ website/scripts/build-assets.mjs
@@ -1,11 +1,13 @@
 import { generateDemoImages } from './generate-img.mjs'
 import { generateOgImage } from './og-image.mjs'
 import { generateChangelog } from './changelog.mjs'
+import { generateShowcaseManifest } from './showcase-manifest.mjs'
 
 export async function generateAssets() {
   await generateDemoImages()
   await generateOgImage()
   await generateChangelog()
+  await generateShowcaseManifest()
 }
 
 if (import.meta.url === `file://${process.argv[1]}`) await generateAssets()
